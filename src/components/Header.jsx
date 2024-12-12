0d2471f57f4a7c1fdbfb718aba35de1696b7e898
TheLegion007 <manisksing@gmail.com>
2024-12-13 01:01:29 +0530

    added components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const image_header = "https://img.freepik.com/free-vector/funny-cartoon-face-design_1095-47.jpg"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img  src={image_header} />
+            <h1> Generate Your Meme ! </h1>
+        </header>
+    )
+}
